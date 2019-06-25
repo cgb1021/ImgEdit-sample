ec036ab4bc0da3e361374262305c016b1f5807c0
chenguibiao <chenguibiao@yy.com>
2019-06-25 11:39:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Sample/>
+  </div>
+</template>
+
+<script>
+import Sample from './components/Sample'
+
+export default {
+  name: 'App',
+  components: {
+    Sample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Sample.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="img-edit">
+    <h1>ImgEdit sample</h1>
+  </div>
+</template>
+
+<script>
+import ImgEdit from 'imgedit'
+export default {
+  name: 'Sample',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    console.log('sample mounted', ImgEdit)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
