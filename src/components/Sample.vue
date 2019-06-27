93d48c5344c6be958fbd36e865270e2da81aacf4
chenguibiao <chenguibiao@yy.com>
2019-06-27 17:51:22 +0800

    存档

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -6,24 +6,56 @@
       <form action="">
         <input type="file" name="" multiple accept="image/*" id="file_input">
       </form>
-      <div class="info">width: height:</div>
+      <div class="info">width: {{file.width}} height: {{file.width}}</div>
       <div class="tools"><Btn text="预览" @click.native="preview"/></div>
     </div>
   </div>
 </template>
 
 <script>
-import ImgEdit, { readFile, loadImg } from 'imgedit'
+import ImgEdit from 'imgedit'
 import message from 'jmessage'
 import SparkMD5 from 'spark-md5'
 import Btn from './Btn'
 let edit
+function getImgBlob (url) {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest()
+    xhr.responseType = 'blob'
+    xhr.onload = () => {
+      const file = xhr.response
+      let name = ''
+      const m = url.match(/[\w.-]+\.(?:jpe?g|png)$/)
+      if (m) name = m[0]
+      else {
+        name = SparkMD5.hash(url)
+        switch (file.type.slice('/')[1]) {
+          case 'png': name = `${name}.png`
+            break
+          default: name = `${name}.jpg`
+            break
+        }
+      }
+      file.name = name
+      resolve(file)
+    }
+    xhr.open('GET', url)
+    // xhr.overrideMimeType('text/plain; charset=x-user-defined')
+    xhr.send(null)
+  })
+}
 export default {
   name: 'sample',
   components: { Btn },
   data () {
     return {
-      files: []
+      file: {
+        name: '',
+        size: 0,
+        width: 0,
+        height: 0,
+        md5: ''
+      }
     }
   },
   mounted () {
@@ -46,12 +78,6 @@ export default {
           }
         } */
       })
-      edit.draw('https://t12.baidu.com/it/u=54104471,2172971201&fm=76')
-      document.getElementById('file_input').addEventListener('change', e => {
-        for (const f of e.target.files) {
-          this.add(f)
-        }
-      })
       const drawRange = document.getElementById('draw_range')
       drawRange.addEventListener('dragenter', e => {
         e.preventDefault()
@@ -66,11 +92,19 @@ export default {
       })
       drawRange.addEventListener('drop', e => {
         e.preventDefault()
-        for (const f of e.dataTransfer.files) {
+        /* for (const f of e.dataTransfer.files) {
           this.add(f)
-        }
+        } */
+        this.add(e.dataTransfer.files[0])
         drawRange.classList.remove('active')
       })
+      document.getElementById('file_input').addEventListener('change', e => {
+        /* for (const f of e.target.files) {
+          this.add(f)
+        } */
+        this.add(e.target.files[0])
+      })
+      this.add('https://t12.baidu.com/it/u=54104471,2172971201&fm=76')
     })
     message.config({
       noClose: true,
@@ -78,7 +112,10 @@ export default {
     })
   },
   methods: {
-    add (file) {
+    async add (file) {
+      if (typeof file === 'string' && /^(?:https?:)?\/\//.test(file)) {
+        file = await getImgBlob(file)
+      }
       if (!/\.(?:png|jpg|jpeg|gif|bmp)$/i.test(file.name)) return false
       const blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice
       const fileReader = new FileReader()
@@ -96,18 +133,20 @@ export default {
         } else {
           // 数据块加载结束
           md5 = spark.end()
-          const dataURL = await readFile(file)
-          const img = await loadImg(dataURL)
-          this.files.push({
+          await edit.open(file)
+          edit.draw()
+          /* this.files.push({
             name: file.name,
             size: file.size,
-            width: img.width,
-            height: img.height,
-            md5,
-            dataURL
-          })
-          edit.draw(img)
-          console.log(this.files[this.files.length - 1])
+            width: edit.width(),
+            height: edit.height(),
+            md5
+          }) */
+          this.file.name = file.name
+          this.file.size = file.size
+          this.file.width = edit.width()
+          this.file.height = edit.height()
+          this.file.md5 = md5
         }
       }
       function loadNext () {
